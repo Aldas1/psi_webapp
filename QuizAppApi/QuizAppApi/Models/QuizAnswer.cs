c47f0090ec3dc60895fe97d85ad3b557c0a1f50e
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-09-18 19:44:05 +0300

    Implement IEquatable in QuizAnswer and remove old code
    We will add it back if the need arises.

--- QuizAppApi/QuizAppApi/Models/QuizAnswer.cs
+++ QuizAppApi/QuizAppApi/Models/QuizAnswer.cs
@@ -1,19 +1,17 @@
 ﻿namespace QuizAppApi.Models
 {
-    public abstract class QuizAnswer
+    public abstract class QuizAnswer : IEquatable<QuizAnswer>
     {
-        public static bool operator==(QuizAnswer a, QuizAnswer b)
-        {
-            return a.IsCorrect(b);
-        }
-
-        public static bool operator!=(QuizAnswer a, QuizAnswer b)
-        {
-            return !a.IsCorrect(b);
-        }
-
         // Compares two answers and returns true if answers are the same (or using some other logic)
         public abstract bool IsCorrect(QuizAnswer answer);
+
+        public bool Equals(QuizAnswer? other)
+        {
+           if (other == null) return false;
+            return this.IsCorrect(other);
+        }
+
         protected QuizAnswer() { }
     }
+
 }
