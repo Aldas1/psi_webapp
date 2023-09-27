236a357b95e157304eceecc75fee95e7b0ee6920
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-09-27 17:49:19 +0300

    Update models Logic is removed and QuizAnswer was removed as well (it was unnecessary and confusing)

---
+++ QuizAppApi/QuizAppApi/Models/Questions/MultipleChoiceQuestion.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizAppApi.Models.Questions
+{
+    public class MultipleChoiceQuestion : Question
+    {
+        ICollection<Option> Options { get; set; }
+        ICollection<Option> CorrectOptionIndexes { get; set; }
+    }
+}
