2049a70e26fb98ac9436b469a2e6b0d495158790
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-09-28 12:00:52 +0300

    Use dependency injection to add QuizService to QuizController

---
+++ QuizAppApi/QuizAppApi/Interfaces/IQuizService.cs
@@ -0,0 +1,12 @@
+﻿using QuizAppApi.DTOs;
+
+namespace QuizAppApi.Interfaces
+{
+    public interface IQuizService
+    {
+        QuizCreationResponseDTO CreateQuiz(QuizCreationRequestDTO request);
+        IEnumerable<QuizResponseDTO> GetQuizzes();
+        IEnumerable<QuestionResponseDTO> GetQuestions(int id);
+        AnswerSubmitResponseDTO SubmitAnswers(int id, List<AnswerSubmitRequestDTO> request);
+    }
+}
---
+++ QuizAppApi/QuizAppApi/Services/QuizService.cs
@@ -0,0 +1,44 @@
+﻿using QuizAppApi.DTOs;
+using QuizAppApi.Interfaces;
+
+namespace QuizAppApi.Services
+{
+    public class QuizService : IQuizService
+    {
+        public QuizCreationResponseDTO CreateQuiz(QuizCreationRequestDTO request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<QuestionResponseDTO> GetQuestions(int id)
+        {
+            throw new NotImplementedException();
+            //var quiz = _quizzes.Find(q => q.Id == id);
+            //if (quiz == null)
+            //    return NotFound();
+            //List<QuestionResponseDTO> questions = new();
+            //foreach (var question in quiz.Questions)
+            //{
+            //    //questions.Add(new QuestionResponseDTO { Text = question.Text, QuestionType = question.QuestionType, QuestionParameters = question.GenerateApiParameters()  });
+            //}
+            //return questions;
+        }
+
+        public IEnumerable<QuizResponseDTO> GetQuizzes()
+        {
+            throw new NotImplementedException();
+            //// TODO: (Maybe) Posibly use LINQ here
+            //List<QuizResponseDTO> quizesResponse = new();
+            //foreach (var quiz in _quizzes)
+            //{
+            //    quizesResponse.Add(new QuizResponseDTO { Name = quiz.Name, Id = quiz.Id });
+            //}
+            //return quizesResponse;
+        }
+
+        public AnswerSubmitResponseDTO SubmitAnswers(int id, List<AnswerSubmitRequestDTO> request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
