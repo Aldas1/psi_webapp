0a85553e643f48de246c8360e3055d3bd0e285f0
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-09-28 10:52:51 +0300

    Comment out code that should be updated We are likely to use services and controller is only going to be used for calling a proper service. QuizStorage should also be rewritten and likely put into a different location.

--- QuizAppApi/QuizAppApi/QuizStorage.cs
+++ QuizAppApi/QuizAppApi/QuizStorage.cs
@@ -14,20 +14,20 @@ namespace QuizAppApi
         }
         private QuizStorage()
         {
-            Quiz citiesQuiz = new Quiz("Cities and Countries", 2);
+            //Quiz citiesQuiz = new Quiz("Cities and Countries", 2);
 
-            // populate with sample data
-            // TODO: Use db
-            citiesQuiz.Questions.Add(
-                new SingleChoiceQuizQuestion("What is the capital of Lithuania?", new List<string>() { "Kaunas", "Vilnius", "Jonava" }, new SingleChoiceQuizAnswer(1))
-            );
-            citiesQuiz.Questions.Add(
-                new SingleChoiceQuizQuestion("What is the capital of Spain?", new List<string>() { "Madrid", "Vilnius", "Jonava" }, new SingleChoiceQuizAnswer(0))
-            );
-            citiesQuiz.Questions.Add(
-                new SingleChoiceQuizQuestion("Do all countries have capitals?", new List<string>() { "Yes", "No" }, new SingleChoiceQuizAnswer(0))
-            );
-            this.Add(citiesQuiz);
+            //// populate with sample data
+            //// TODO: Use db
+            //citiesQuiz.Questions.Add(
+            //    new SingleChoiceQuizQuestion("What is the capital of Lithuania?", new List<string>() { "Kaunas", "Vilnius", "Jonava" }, new SingleChoiceQuizAnswer(1))
+            //);
+            //citiesQuiz.Questions.Add(
+            //    new SingleChoiceQuizQuestion("What is the capital of Spain?", new List<string>() { "Madrid", "Vilnius", "Jonava" }, new SingleChoiceQuizAnswer(0))
+            //);
+            //citiesQuiz.Questions.Add(
+            //    new SingleChoiceQuizQuestion("Do all countries have capitals?", new List<string>() { "Yes", "No" }, new SingleChoiceQuizAnswer(0))
+            //);
+            //this.Add(citiesQuiz);
         }
 
     }
