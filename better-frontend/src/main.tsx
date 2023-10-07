b8c13a3e039def30869c5bb2d3980f912d74c133
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-07 14:27:51 +0300

    Use system color mode

--- better-frontend/src/main.tsx
+++ better-frontend/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
