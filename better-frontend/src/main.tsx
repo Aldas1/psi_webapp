da7a2a7d50c97d7e81a176baa281eee2676ea465
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-08 19:09:34 +0300

    Use tanstack react query lib

--- better-frontend/src/main.tsx
+++ better-frontend/src/main.tsx
@@ -4,13 +4,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router";
 import theme from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
