7de9cd3c40456b59e2eeef59cdff5889da60f730
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-08 19:24:51 +0300

    Update home page

--- better-frontend/src/pages/Home/index.tsx
+++ better-frontend/src/pages/Home/index.tsx
@@ -1,5 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { getQuizzes } from "../../api/quizzes";
+import { Spinner } from "@chakra-ui/spinner";
+import { Card, CardHeader } from "@chakra-ui/card";
+import { SimpleGrid } from "@chakra-ui/layout";
+import { Link as ReactRouterLink } from "react-router-dom";
 function Home() {
-  return "Home";
+  const { isLoading, data, isError } = useQuery({
+    queryKey: ["quizzes"],
+    queryFn: getQuizzes,
+  });
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return "Unexpected Error!";
+  }
+
+  return (
+    <SimpleGrid gap="2" templateColumns="repeat(auto-fit, minmax(240px, 1fr))">
+      {data.map((quiz) => (
+        <ReactRouterLink to={`/quizzes/${quiz.id}`} key={quiz.id}>
+          <Card>
+            <CardHeader>{quiz.name}</CardHeader>
+          </Card>
+        </ReactRouterLink>
+      ))}
+    </SimpleGrid>
+  );
 }
 
 export default Home;
