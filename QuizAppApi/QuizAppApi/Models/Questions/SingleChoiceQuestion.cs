f3d6f9c6a46f4dc34e72c9b7f4877ebbe1c31521
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-10-09 22:03:16 +0300

    Fix model warnings

--- QuizAppApi/QuizAppApi/Models/Questions/SingleChoiceQuestion.cs
+++ QuizAppApi/QuizAppApi/Models/Questions/SingleChoiceQuestion.cs
@@ -4,8 +4,8 @@ namespace QuizAppApi.Models.Questions
 {
     public class SingleChoiceQuestion : Question
     {
-        public ICollection<Option> Options { get; set; }
-        public Option CorrectOption { get; set; }
+        public ICollection<Option> Options { get; set; } = new List<Option>();
+        public Option CorrectOption { get; set; } = new Option();
         public override QuestionType Type { get => QuestionType.SingleChoiceQuestion; }
     }
 }
