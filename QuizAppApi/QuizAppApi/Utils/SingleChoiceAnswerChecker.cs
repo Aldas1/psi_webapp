5387a4f9dadbb0f31231c8b4a0649ecbd1d93931
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-10-10 18:08:37 +0300

    Added optional parameters

--- QuizAppApi/QuizAppApi/Utils/SingleChoiceAnswerChecker.cs
+++ QuizAppApi/QuizAppApi/Utils/SingleChoiceAnswerChecker.cs
@@ -4,8 +4,12 @@ namespace QuizAppApi.Utils
 {
     public static class SingleChoiceAnswerChecker
     {
-        public static bool IsCorrect(SingleChoiceQuestion question, Option answer)
+        public static bool IsCorrect(SingleChoiceQuestion question, Option answer, bool lowercaseComparison = false)
         {
+            if (lowercaseComparison)
+            {
+                return answer.Name.ToLower() == question.CorrectOption.Name.ToLower();
+            }
             return answer.Name == question.CorrectOption.Name;
         }
     }
