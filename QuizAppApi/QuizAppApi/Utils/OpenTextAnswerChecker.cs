6874afbe93e62d641f566d767d635a86a400de41
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-10-12 15:26:03 +0300

    Revert "Fixed mistake"
    This reverts commit 02afb132b40a8fb6c72f430785d2c206a8a2afca.

--- QuizAppApi/QuizAppApi/Utils/OpenTextAnswerChecker.cs
+++ QuizAppApi/QuizAppApi/Utils/OpenTextAnswerChecker.cs
@@ -4,12 +4,15 @@ namespace QuizAppApi.Utils
 {
     public static class OpenTextAnswerChecker
     {
-        public static bool IsCorrect(OpenTextQuestion question, string answerPara, bool useLowercaseComparison = false, bool trimWhitespace = false)
+        public static bool IsCorrect(OpenTextQuestion question, string answer, bool useLowercaseComparison = false, bool trimWhitespace = false)
         {
-            var answer = trimWhitespace ? answerPara.Trim() : answerPara;
-            var correctAnswer = trimWhitespace ? question.CorrectAnswer.Trim() : question.CorrectAnswer;
-            
-            return useLowercaseComparison ? answer.ToLower() == correctAnswer.ToLower() : answer == correctAnswer;
+            var a = trimWhitespace ? answer.Trim() : answer;
+            var b = trimWhitespace ? question.CorrectAnswer.Trim() : question.CorrectAnswer;
+            if (useLowercaseComparison)
+            {
+                return a == b;
+            }
+            return question.CorrectAnswer == answer;
         }
     }
 }
