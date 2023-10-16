82c1f92e30d46214bae5a548ef1ef1f631eb72f4
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-10-17 00:45:08 +0300

    Added method skeleton in QuizService

--- QuizAppApi/QuizAppApi/Interfaces/IQuizService.cs
+++ QuizAppApi/QuizAppApi/Interfaces/IQuizService.cs
@@ -5,6 +5,7 @@ namespace QuizAppApi.Interfaces
     public interface IQuizService
     {
         QuizCreationResponseDTO CreateQuiz(QuizCreationRequestDTO request);
+        QuizCreationResponseDTO UpdateQuiz(QuizCreationRequestDTO editRequest);
         IEnumerable<QuestionResponseDTO>? GetQuestions(int id);
         IEnumerable<QuizResponseDTO> GetQuizzes();
         QuizResponseDTO? GetQuiz(int id);
