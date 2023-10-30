30584ef64152a79c41c92a717b5efb3e81202e65
Danielius Podbielski <danieliuspodb@gmail.com>
2023-10-30 21:44:50 +0200

    Change layout

--- QuizAppApi/Tests/OpenTextQuestionDTOConverterServiceTest.cs
+++ QuizAppApi/Tests/OpenTextQuestionDTOConverterServiceTest.cs
@@ -3,69 +3,45 @@ using QuizAppApi.DTOs;
 using QuizAppApi.Exceptions;
 using QuizAppApi.Models.Questions;
 using QuizAppApi.Services;
-using System.Linq;
 
 namespace Tests;
 
 [TestFixture]
 public class OpenTextQuestionDTOConverterServiceTests
 {
-    private SingleChoiceQuestionDTOConverterService _singleChoiceConverter;
-    private MultipleChoiceQuestionDTOConverterService _multipleChoiceConverter;
     private OpenTextQuestionDTOConverterService _openTextConverter;
 
     [SetUp]
     public void Setup()
     {
-        _singleChoiceConverter = new SingleChoiceQuestionDTOConverterService();
-        _multipleChoiceConverter = new MultipleChoiceQuestionDTOConverterService();
         _openTextConverter = new OpenTextQuestionDTOConverterService();
     }
 
     [Test]
     public void CreateFromParameters_ReturnsOpenTextQuestion()
     {
-        // Arrange
         var questionDTO = new QuestionParametersDTO
         {
             CorrectText = "ValidAnswer?"
         };
 
-        // Act
         var result = _openTextConverter.CreateFromParameters(questionDTO);
 
-        // Assert
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<OpenTextQuestion>(result);
         Assert.AreEqual("ValidAnswer?", result.CorrectAnswer);
     }
 
     [Test]
-    public void CreateFromParameters_ThrowsDTOConversionException()
-    {
-        // Arrange
-        var questionDTO = new QuestionParametersDTO
-        {
-            CorrectText = "#InvalidAnswer"
-        };
-
-        // Act & Assert
-        Assert.Throws<DTOConversionException>(() => _openTextConverter.CreateFromParameters(questionDTO));
-    }
-
-    [Test]
     public void GenerateParameters_ReturnsQuestionParametersDTO()
     {
-        // Arrange
         var question = new OpenTextQuestion
         {
             CorrectAnswer = "ValidAnswer?"
         };
 
-        // Act
         var result = _openTextConverter.GenerateParameters(question);
 
-        // Assert
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<QuestionParametersDTO>(result);
         Assert.AreEqual("ValidAnswer?", result.CorrectText);
--- QuizAppApi/Tests/SingleChoiceQuestionDTOConverterServiceTest.cs
+++ QuizAppApi/Tests/SingleChoiceQuestionDTOConverterServiceTest.cs
@@ -3,7 +3,6 @@ using QuizAppApi.DTOs;
 using QuizAppApi.Exceptions;
 using QuizAppApi.Models.Questions;
 using QuizAppApi.Services;
-using System.Linq;
 
 namespace Tests;
 
@@ -11,56 +10,48 @@ namespace Tests;
 public class SingleChoiceQuestionDTOConverterServiceTests
 {
     private SingleChoiceQuestionDTOConverterService _singleChoiceConverter;
-    private MultipleChoiceQuestionDTOConverterService _multipleChoiceConverter;
-    private OpenTextQuestionDTOConverterService _openTextConverter;
 
     [SetUp]
     public void Setup()
     {
         _singleChoiceConverter = new SingleChoiceQuestionDTOConverterService();
-        _multipleChoiceConverter = new MultipleChoiceQuestionDTOConverterService();
-        _openTextConverter = new OpenTextQuestionDTOConverterService();
     }
 
     [Test]
     public void CreateFromParameters_ReturnsSingleChoiceQuestion()
     {
-        // Arrange
         var questionDTO = new QuestionParametersDTO
         {
             Options = new List<string> { "Option1", "Option2", "Option3" },
             CorrectOptionIndex = 0
         };
 
-        // Act
         var result = _singleChoiceConverter.CreateFromParameters(questionDTO);
 
-        // Assert
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<SingleChoiceQuestion>(result);
         Assert.AreEqual(3, result.Options.Count);
-        Assert.IsTrue(result.Options.All(option => option.Name == "Option1" || option.Name == "Option2" || option.Name == "Option3"));
+        Assert.IsTrue(result.Options.Any(option => option.Name == "Option1"));
+        Assert.IsTrue(result.Options.Any(option => option.Name == "Option2"));
+        Assert.IsTrue(result.Options.Any(option => option.Name == "Option3"));
         Assert.IsTrue(result.Options.Any(option => option.Correct));
     }
 
     [Test]
     public void CreateFromParameters_ThrowsDTOConversionException()
     {
-        // Arrange
         var questionDTO = new QuestionParametersDTO
         {
             Options = new List<string> { "Option1", "Option2", "Option3" },
             CorrectOptionIndex = 3
         };
 
-        // Act & Assert
         Assert.Throws<DTOConversionException>(() => _singleChoiceConverter.CreateFromParameters(questionDTO));
     }
 
     [Test]
     public void GenerateParameters_ReturnsQuestionParametersDTO()
     {
-        // Arrange
         var question = new SingleChoiceQuestion
         {
             Options = new List<Option>
@@ -70,14 +61,13 @@ public class SingleChoiceQuestionDTOConverterServiceTests
             }
         };
 
-        // Act
         var result = _singleChoiceConverter.GenerateParameters(question);
 
-        // Assert
         Assert.IsNotNull(result);
         Assert.IsInstanceOf<QuestionParametersDTO>(result);
         Assert.AreEqual(2, result.Options.Count);
-        Assert.IsTrue(result.Options.All(option => option == "Option1" || option == "Option2"));
+        Assert.IsTrue(result.Options.Any(option => option == "Option1"));
+        Assert.IsTrue(result.Options.Any(option => option == "Option2"));
         Assert.AreEqual(0, result.CorrectOptionIndex);
     }
 }
