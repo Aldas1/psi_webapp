796f2b5ee844512ed8665fb894c521c6a7aeb2be
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-11-04 20:41:26 +0200

    make password and username required

--- QuizAppApi/QuizAppApi/DTOs/UserRequestDTO.cs
+++ QuizAppApi/QuizAppApi/DTOs/UserRequestDTO.cs
@@ -4,8 +4,10 @@ namespace QuizAppApi.DTOs;
 
 public class UserRequestDTO
 {
+    [Required]
     public string Username { get; init; }
 
+    [Required]
     [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$",
         ErrorMessage =
             "Password must contain at least 8 characters including at least one digit, lowercase and uppercase letter")]
