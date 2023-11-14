369abdc447e99ff5ac7a2c996a4fdc4fb77f2675
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-11-14 02:02:18 +0200

    async quiz controller

--- QuizAppApi/QuizAppApi/Controllers/QuizController.cs
+++ QuizAppApi/QuizAppApi/Controllers/QuizController.cs
@@ -16,21 +16,21 @@ public class QuizController : ControllerBase
     }
 
     [HttpPost]
-    public ActionResult<QuizManipulationResponseDto> CreateQuiz([FromBody] QuizManipulationRequestDto request)
+    public async Task<ActionResult<QuizManipulationResponseDto>> CreateQuiz([FromBody] QuizManipulationRequestDto request)
     {
-        return _quizService.CreateQuizAsync(request);
+        return await _quizService.CreateQuizAsync(request);
     }
 
     [HttpPut("{id}")]
-    public ActionResult<QuizManipulationResponseDto> UpdateQuiz(int id, [FromBody] QuizManipulationRequestDto updateRequest)
+    public async Task<ActionResult<QuizManipulationResponseDto>> UpdateQuiz(int id, [FromBody] QuizManipulationRequestDto updateRequest)
     {
-        return _quizService.UpdateQuizAsync(id, updateRequest);
+        return await _quizService.UpdateQuizAsync(id, updateRequest);
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<QuizResponseDto>> GetQuizzes()
+    public async Task<ActionResult<IEnumerable<QuizResponseDto>>> GetQuizzes()
     {
-        var quizzes = _quizService.GetQuizzesAsync();
+        var quizzes = await _quizService.GetQuizzesAsync();
         if (quizzes == null)
         {
             return NotFound();
@@ -39,9 +39,9 @@ public class QuizController : ControllerBase
     }
 
     [HttpGet("{id}")]
-    public ActionResult<QuizResponseDto> GetQuiz(int id)
+    public async Task<ActionResult<QuizResponseDto>> GetQuiz(int id)
     {
-        var quiz = _quizService.GetQuizAsync(id);
+        var quiz = await _quizService.GetQuizAsync(id);
         if (quiz == null)
         {
             return NotFound();
@@ -51,9 +51,9 @@ public class QuizController : ControllerBase
     }
 
     [HttpDelete("{id}")]
-    public ActionResult DeleteQuiz(int id)
+    public async Task<ActionResult> DeleteQuiz(int id)
     {
-        bool response = _quizService.DeleteQuizAsync(id);
+        bool response = await _quizService.DeleteQuizAsync(id);
         if (response)
         {
             return Ok();
