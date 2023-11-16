89b770a01dc9f328b8e6cc557c8984b9d8068359
Motiejus Šveikauskas <118689022+Motiejukaas@users.noreply.github.com>
2023-11-16 20:49:18 +0200

    some minor changes to the delegate

--- QuizAppApi/QuizAppApi/Events/EventBase.cs
+++ QuizAppApi/QuizAppApi/Events/EventBase.cs
@@ -1,10 +1,10 @@
 namespace QuizAppApi.Events;
 
+public delegate Task AsyncEventHandler<TEventArgs>(object sender, TEventArgs args);
+
 public class EventBase<TEventArgs> where TEventArgs : EventArgs
 {
-    public delegate Task EventDelegate(object  sender, TEventArgs args);
-
-    public static event EventDelegate? Event;
+    public static event AsyncEventHandler<TEventArgs>? Event;
 
     public static void Raise(object sender, TEventArgs args)
     {
