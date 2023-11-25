3037e42ebf96b3cde0f3c89111b233f86d18587e
Kanstantinas Piatrashka <piatrashkakanstantinass@gmail.com>
2023-11-25 12:16:27 +0200

    fix tests

--- QuizAppApi/QuizAppApi/Interfaces/IQuizService.cs
+++ QuizAppApi/QuizAppApi/Interfaces/IQuizService.cs
@@ -4,7 +4,7 @@ using QuizAppApi.Models;
 namespace QuizAppApi.Interfaces;
 public interface IQuizService
 {
-    Task<QuizManipulationResponseDto> CreateQuizAsync(QuizManipulationRequestDto request, User? user);
+    Task<QuizManipulationResponseDto> CreateQuizAsync(QuizManipulationRequestDto request, User? user = null);
     Task<QuizManipulationResponseDto> UpdateQuizAsync(int id, QuizManipulationRequestDto editRequest);
     Task<IEnumerable<QuizResponseDto>> GetQuizzesAsync();
     Task<QuizResponseDto?> GetQuizAsync(int id);
